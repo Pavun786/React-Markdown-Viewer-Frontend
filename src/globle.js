9e36756b0f8f769f243d6815df6e6b65a1ec7d19
Pavun786 <pulsarpavuns786@gmail.com>
2023-05-04 05:52:26 -0700

    url_Change

--- src/globle.js
+++ src/globle.js
@@ -1,3 +1,2 @@
-
-// export const API = "https://react-markdown-viewer-backend.vercel.app" 
-export const API = "http://localhost:4000"
+// export const API = "http://localhost:4000"
+ export const API = "https://react-markdown-viewer-backend.vercel.app" 
